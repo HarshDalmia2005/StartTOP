b009576b8311bcbbaf97780cc35e482d2d7d59b1
Harsh Dalmia <harshdalmia20212081@gmail.com>
2025-04-21 19:41:24 +0530

    Sanity schema setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono,Work_Sans } from "next/font/google";
 import "./globals.css";
+import "easymde/dist/easymde.min.css";  
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
